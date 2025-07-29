d6f86750e2f897326aba921288de487592af2d4e
Seanon1 <seangachango@gmail.com>
2025-07-29 12:58:56 +0300

    using map to make lists

---
+++ src/components/Projects.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function Projects() { 
+
+    const projects = [
+        {
+            id:1,
+            name:"Recipe App",
+            description:"A simple recipe app that has different food menu.",
+            techStack:["Javascript", "HTML", "CSS", "Tailwind"],
+            module:'Javascript' 
+        },
+        {
+            id:2,
+            name:"Menu App",
+            description:"A simple menu app that is interactive and has many different options.",
+            techStack:["Javascript"],
+            module:'Javascript' 
+        },
+        {
+            id:3,
+            name:"Weather App",
+            description:"A simple weather app that displays the current weather of a city or state.",
+            techStack:["Javascript", "HTML", "CSS", "Tailwind"],
+            module:'Javascript' 
+        }
+    ]
+    
+  return (
+    <div>
+        <h2>Projects</h2>
+
+        <ul>
+            {projects.map(project => (
+                <li key={project.id}>
+                    <h3>{project.name}</h3>
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default Projects
